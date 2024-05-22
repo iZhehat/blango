4a4a48db1e6e6a7c9fd0b9d511a9526fe55f1b67
Zhehat Sadeeq <zhehat.younis@korektel.com>
2024-05-22 16:39:40 +0000

    Finish crispy forms

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.utils import timezone
-from . import models
+from django.utils import timezone 
+from django.shortcuts import redirect
+
+from . import models, forms
 
 def index(request):
   posts = models.Post.objects.filter(published_at__lte=timezone.now())
@@ -8,4 +10,18 @@ def index(request):
 
 def post_detail(request, slug):
   post = get_object_or_404(models.Post, slug=slug)
-  return render(request, "blog/post-detail.html", {"post": post})
+  if request.user.is_active:
+    if request.method == "POST":
+      comment_form = forms.CommentForm(request.POST)
+
+      if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.content_object = post
+        comment.creator = request.user
+        comment.save()
+        return redirect(request.path_info)
+    else:
+      comment_form = forms.CommentForm
+  else:
+    comment_form = None
+  return render(request, "blog/post-detail.html", {"post": post, "comment_form": comment_form})
