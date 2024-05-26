fa2b891917e12506e5c5adb147b1e2ee423f7816
Zhehat Sadeeq <zhehat.younis@korektel.com>
2024-05-26 08:35:58 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -113,6 +113,46 @@ class Dev(Configuration):
         },
     ]
 
+    #LOGGING
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "filters": {
+            "require_debug_false": {
+                "()": "django.utils.log.RequireDebugFalse",
+            },
+        },
+        "formatters": {
+            "verbose": {
+                "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+                "style": "{",
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "verbose",
+            },
+            "mail_admins": {
+                "level": "ERROR",
+                "class": "django.utils.log.AdminEmailHandler",
+                "filters": ["require_debug_false"],
+            },
+        },
+        "loggers": {
+            "django.request": {
+                "handlers": ["mail_admins"],
+                "level": "ERROR",
+                "propagate": True,
+            },
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    }
+
 
     # Internationalization
     # https://docs.djangoproject.com/en/3.2/topics/i18n/
