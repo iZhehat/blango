b0ea8ac1cd14cc000a40fc2f090e0b1d41d8a6b4
Zhehat Sadeeq <zhehat.younis@korektel.com>
2024-05-27 19:32:47 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -60,13 +60,27 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
         "debug_toolbar",
         'blango_auth',
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
+        'allauth',
+        'allauth.account',
+        'allauth.socialaccount',
+        'allauth.socialaccount.providers.google',
     ]
+
+    # Allauth config
+    SITE_ID = 1
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
+    # Use it if you want that allauth handle the two-step-registrationn too
+    #ACCOUNT_EMAIL_VERIFICATION = True 
 
     MIDDLEWARE = [
         "debug_toolbar.middleware.DebugToolbarMiddleware",
