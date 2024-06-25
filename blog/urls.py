94a0cf11425895b2ff1a94f9b314ba4badd2aa7f
Zhehat Sadeeq <zhehat.younis@korektel.com>
2024-06-25 09:53:21 +0000

    Finish JS intro and fundamentals

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ import blog.views
 urlpatterns = [
   path("", blog.views.index, name='blog_index'),
   path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
-  path("ip/", blog.views.get_ip)
+  path("ip/", blog.views.get_ip),
+  path("post-table/", blog.views.post_table, name="blog-post-table"),
 ]
